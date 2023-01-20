f8b82b2f7b411205379f361ea0d2d3c97908feb0
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-01-20 17:39:54 +0700

    добавлен js

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,22 @@
-const menuLinks = document.querySelectorAll('.header__menu-option');
-const moreButton = document.querySelector('.lead__more-btn');
-const menuButton = document.querySelector('.header__menu-btn');
 const menu = document.querySelector('.header__menu');
+const menuButton = document.querySelector('.header__menu-btn');
+const menuLinks = menu.querySelectorAll('a[href^="#"]');
+const mobileMenuOptions = document.querySelectorAll('.header__menu-option');
+const moreButton = document.querySelector('.lead__more-btn');
 const bikeCategories = document.querySelectorAll('.bikes__category');
 const submitButton = document.querySelector('.footer__submit-btn');
+const menuList = document.querySelector('.header__menu-options');
 
-for (const link of menuLinks) {
+
+// скрывается мобильное меню при переходе по якорной ссылке
+for (const link of mobileMenuOptions) {
   link.addEventListener('click', () => {
-    menuButton.checked = false;
+    menuList.style.display = "none";
   });
 }
 
-for (const category of bikeCategories) {
-  category.addEventListener('click', (event) => {
-    event.preventDefault();
-  });
-}
-
-moreButton.addEventListener('click', (event) => {
-  event.preventDefault();
-});
-
-menu.querySelectorAll('a[href^="#"]').forEach(link => {
+//плавная прокрутка страницы при переходе по якорной ссылке из меню с учетом высоты fixed элемента
+for (const link of menuLinks) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     let href = this.getAttribute('href').substring(1);
@@ -34,13 +29,39 @@ menu.querySelectorAll('a[href^="#"]').forEach(link => {
       behavior: 'smooth'
     });
   });
+}
+
+// отмена обновления страницы при нажатии на категорию без полной реализации блока
+for (const category of bikeCategories) {
+  category.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+}
+
+//появление меню при нажатии на кнопку вызова
+menuButton.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('header__menu-close')) {
+    menuList.style.display = "flex";
+  } else {
+    menuList.style.display = "none";
+  }
 });
 
+//изменение кнопки вызова меню
+menuButton.addEventListener('click', (event) => {
+  event.target.classList.toggle('header__menu-close');
+});
+
+// отмена обновления страницы при нажатии на кнопку Подробнее без ссылки
+moreButton.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+//появление кнопки отправки формы при активации поля ввода e-mail
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('footer__input-email')) {
     submitButton.style.display = "block";
-  }
-  else {
+  } else {
     submitButton.style.display = "none";
   }
 });
